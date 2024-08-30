efa149925308c2deec2db8efd01042d1d34761b6
Abhash Tiwari <abhashhtiwary7610@gmail.com>
2024-08-31 01:26:19 +0530

    Server Started

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,14 @@
-console.log("yo")
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+
+app.use(express.json());
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
